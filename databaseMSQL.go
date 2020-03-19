c81216e1c9c2cd73bd521b5e0fae237ccde3046e
vinita2000 <vinitayadavlkw225@gmail.com>
2020-03-19 20:57:50 +0530

    Add files via upload

---
+++ databaseMSQL.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http")
+
+
+func index_handler(w http.ResponseWriter,r *http.Request) {
+
+	fmt.Fprintf(w,"WHoa, Go is neat")
+}
+
+func main(){
+	http.HandleFunc("/",index_handler)
+	http.ListenAndServe(":8000",nil)
+}
+
+
